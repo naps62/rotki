4762de4bf1a35ee23ea52a49960f5f45adaa2abc
Miguel Palhas <mpalhas@gmail.com>
2025-07-16 16:33:50 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -2,46 +2,97 @@
   description = "rotki project with uv-managed virtualenv";
 
   inputs = {
-    nixpkgs.url          = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url      = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust.url = "github:oxalica/rust-overlay";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      rust,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        unstable = nixpkgs-unstable.legacyPackages.${system};
+        overlays = [ (import rust) ];
+        pkgs = import nixpkgs { inherit system overlays; };
 
-        python311         = pkgs.python311;
-        nodejsWithPython  = pkgs.nodejs_22.override { python3 = python311; };
-        nodePackages      = pkgs.nodePackages.override { nodejs = nodejsWithPython; };
-        pnpmWithPython311 = unstable.pnpm;
+        # nodejs = pkgs.nodejs_22;
+        # python = pkgs.python311.override { nodejs = pkgs.nodejs_22;};
+        # nodejsWithPython = pkgs.nodejs_22.override { python3 = python311; };
+        # nodePackages = pkgs.nodePackages.override { nodejs = nodejsWithPython; };
+        # pnpmWithPython311 = pkgs.pnpm;
 
-          buildInputs = [
-            pkgs.gcc
-            pkgs.stdenv.cc.cc.lib
-            pkgs.bash
-            pkgs.git
-            pkgs.lzma
-            pnpmWithPython311
-            python311   # interpreter uv will bind into the venv
-            pkgs.uv     # blazing-fast venv + installer
-          ];
+        commonDeps = with pkgs; [
+          gcc
+          stdenv.cc.cc.lib
+          bash
+          git
+          xz
+          python311
+          nodejs_22
+          pnpm
+          uv
+        ];
 
-        devShell = pkgs.mkShell {
-          name = "rotki-dev";
-          inherit buildInputs;
+        buildDeps = [ ];
 
-          shellHook = ''
-            # Installs deps and auto-creates .venv if missing
-            uv sync
-
-            # Install frontend dependencies
-            cd frontend
-            pnpm install
-            cd ..
-          '';
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
         };
-      in { inherit devShell; });
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+
+        # build colibri crate
+        colibri = rustPlatform.buildRustPackage (finalAttrs: {
+          pname = "colibri";
+          version = "0.1.0";
+
+          src = ./colibri;
+
+# tests currently try to create a database on a parent dir
+# I can't seem to get around this, previously tried with:
+#   src = ./.
+#   cargoRoot = "./colibri";
+# in order to mount the whole repo, but this was running into issues I couldn't figure out
+
+          doCheck = false;
+
+          buildInputs = with pkgs; [ openssl ];
+          nativeBuildInputs = with pkgs; [ pkg-config perl ];
+
+          cargoDeps = rustPlatform.fetchCargoVendor {
+            inherit (finalAttrs)
+              pname
+              version
+              src
+              ;
+            hash = "sha256-CktDDoDkuJYKyTtNWTQPSz2Dpi8HeQZ11blZqjU6P40=";
+          };
+
+          patchPhase = ''
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp -r /build/colibri $out/bin
+          '';
+        });
+
+      in
+      {
+        packages.default = colibri;
+
+        devShells.default = pkgs.mkShell {
+          name = "rotki-dev";
+          buildInputs = commonDeps;
+        };
+      }
+    );
 }
